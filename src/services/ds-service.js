1b46f92904250960bbbde486479d14d2ed58f58b
Hirschfeld Gerhard, ENT-DES-BPE-DI-WUI <Gerhard.Hirschfeld@swisscom.com>
2017-05-10 16:00:47 +0200

    add basic service structure

---
+++ src/services/ds-service.js
@@ -0,0 +1,33 @@
+
+
+module.exports = {
+
+    /**
+     *
+     */
+    getOAuthToken: () => {
+
+    },
+
+    /**
+     *
+     */
+    getUserInfo: (token, uri) => {
+
+    },
+
+    /**
+     *
+     */
+    getSignInfo: (token, uri) => {
+
+    },
+
+    /**
+     *
+     */
+    postCompleteSignInfo: (token, uri, signature, info) => {
+
+    }
+
+};
